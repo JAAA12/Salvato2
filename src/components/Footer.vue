48c4cba72e55ea842bd3f4b95c8d6c7ad89b1545
Laura <laurasofiaayalamartinez@gmail.com>
2022-11-23 22:19:30 -0500

    Arreglo de footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -23,17 +23,17 @@
                       <ul>
                           <li>
                               <a href="http://facebook.com" target="_blank">
-                                  <img src="img/imgface.png" width="50" height="50">
+                                  <img src="../assets/imgface.png" width="50" height="50">
                               </a>
                           </li>
                           <li>
                               <a href="http://instagram.com" target="_blank">
-                                  <img src="img/imginsta.png" width="50" height="50">
+                                  <img src="../assets/imginsta.png" width="50" height="50">
                               </a>
                           </li>
                           <li>
                               <a href="https://www.whatsapp.com/" target="_blank">
-                                  <img src="img/imgwhatsapp.png" width="50" height="50">
+                                  <img src="../assets/imgwhatsapp.png" width="50" height="50">
                               </a>
                           </li>
                       </ul>
@@ -45,12 +45,23 @@
 </template>
 
 <style>
+
 .footer {
+  background-color:rgb(113, 13, 13);
+  border-top: rgb(113, 13, 13);/*utilizan para establecer de forma abreviada el valor de una o más propiedades individuales.*/
+  padding-top: 3rem;/*establece el espacio de relleno requerido en la parte superior del elemento*/
+  bottom:-100%;/*especifica parte de la posición de un elemento (posicionado - es decir, con una posición determinada por código).*/
+  width: 100%;/*especifica la anchura del area de contenido de un elemento.*/
+  box-shadow: 0px -18px 36px -16px rgba(0,0,0,0.52);/*permite proyectar una sombra difuminada desde el marco de casi cualquier elemento.*/
+  padding: 3rem 0;/*establece el espacio de relleno requerido por todos los lados de un elemento.*/
+  margin-top: 40px;
+}
+.footer .contenedor {
   display: grid;/*especifica si un elemento es tratado como block or inline element y el diseño usado por sus hijos, como flow layout(Diseño de Flujo), grid(Cuadricula) o flex(Flexible). Formalmente la propiedad display establece los tipos de visualización interna y externa de un elemento.*/
   grid-template-columns: repeat(auto-fit, minmax(200px, 1fr) );/*define el nombre de las líneas y las funciones de tamaño de línea de grid columns.*/
   grid-gap: 1rem;/*nos permite determinar los espaciados entre los elementos.*/
 }
-.footer .container2 {
+.footer .container {
   padding: 1rem;
 }
 
@@ -106,4 +117,27 @@
 .sociales ul li span{
   display: none;
 }
+
+footer,
+menu,
+nav
+{
+  display: block;
+}
+
+
+/* Larger than mobile */
+@media (min-width: 400px) {}
+
+/* Larger than phablet (also point when grid becomes active) */
+@media (min-width: 550px) {}
+
+/* Larger than tablet */
+@media (min-width: 750px) {}
+
+/* Larger than desktop */
+@media (min-width: 1000px) {}
+
+/* Larger than Desktop HD */
+@media (min-width: 1200px) {}
 </style>
